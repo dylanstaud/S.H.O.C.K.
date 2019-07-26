4c894fcbabda827b606443875289580a0deca3a1
Elias Shahzad Bakhtani <elias.bakhtani@sjsu.edu>
2019-07-26 12:22:04 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import webapp2
+import os
+
+
+jinja_current_directory =  jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class FortuneHandler(webapp2.RequestHandler):
+    def get(self):
+        random_fortune = "hi"
+        results_template =jinja_current_directory.get_template("index.html")
+        # In part 2, instead of returning this string,
+        # make a function call that returns a random fortune.
+        self.response.write(results_template.render(random_fortune))
+
+
+app = webapp2.WSGIApplication([
+    # This line routes the main url ('/')  - also know as
+    # The root route - to the Fortune Handler
+    ('/', FortuneHandler),
+ #maps '/predict' to the FortuneHandler
+], debug=True)
