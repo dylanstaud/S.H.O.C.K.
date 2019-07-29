5e4bc884ef323e8f77ea67bf945c4281209da7af
Elias Shahzad Bakhtani <elias.bakhtani@sjsu.edu>
2019-07-29 11:43:18 -0700

    sidebar edit

--- main.py
+++ main.py
@@ -12,15 +12,23 @@ jinja_current_directory =  jinja2.Environment(
 class FortuneHandler(webapp2.RequestHandler):
     def get(self):
         random_fortune = "hi"
-        results_template =jinja_current_directory.get_template("index.html")
+        results_template =jinja_current_directory.get_template("templates/index.html")
         # In part 2, instead of returning this string,
         # make a function call that returns a random fortune.
-        self.response.write("hi")
+        self.response.write(results_template.render())
+
+class DropDown(webapp2.RequestHandler):
+    def get(self):
+        rresults_template =jinja_current_directory.get_template("templates/index2.html")
+        # In part 2, instead of returning this string,
+        # make a function call that returns a random fortune.
+        self.response.write(rresults_template.render())
 
 
 app = webapp2.WSGIApplication([
     # This line routes the main url ('/')  - also know as
     # The root route - to the Fortune Handler
     ('/', FortuneHandler),
+    ('/2', DropDown)
  #maps '/predict' to the FortuneHandler
 ], debug=True)
